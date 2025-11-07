c96db7fe340ab0404ee7ce358e777151cfd4f197
Saanj Patel <saanjpatel@ufl.edu>
2025-11-07 14:01:09 -0500

    changed lambda function

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,47 +1,35 @@
 using System.Text;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
+using Amazon.Lambda.APIGatewayEvents
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace EsepWebhook;
 
 public class Function
 {
     
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(object input, ILambdaContext context)
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
     {
-        context.Logger.LogInformation($"FunctionHandler received: {input}");
+        dynamic deserialize = JsonConvert.DeserializeObject(input.Body);
 
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-
-        // Testing from postman, you can use this code to test the function
-        /*
-        context.Logger.LogInformation($"Body: {json.body}");
-        dynamic body = JsonConvert.DeserializeObject<dynamic>(json.body.ToString());
-        context.Logger.LogInformation($"Issue: {body.issue}");
-        context.Logger.LogInformation($"Html: {body.issue.html_url}");
-        string payload = $"{{'text':'Issue Created: {body.issue.html_url}'}}";
-        */
-        
-        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+        string payload = JsonConvert.SerializeObject(new { text = $"Issue Created: {deserialize.issue.html_url}" });
 
         var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
-        {
-            Content = new StringContent(payload, Encoding.UTF8, "application/json")
-        };
+        var environmentVariable = Environment.GetEnvironmentVariable("SLACK_URL");
+        var content = new StringContent(payload, Encoding.UTF8, "application/json");
     
-        var response = client.Send(webRequest);
-        using var reader = new StreamReader(response.Content.ReadAsStream());
-            
-        return reader.ReadToEnd();
+        var response = client.PostAsync(
+            environmentVariable,
+            content
+        ).Result;
+        var proxyResponse = new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = response.Content.ReadAsStringAsync().Result,
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
+
+        return proxyResponse;
     }
 }
--- EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
+++ EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ecfe4bffcf514a281d811252102756803667d1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
